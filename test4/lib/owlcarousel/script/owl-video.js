cbc7bc51377b24fa302a4207e47ab4b63a9ccb01
Pininfa <36874933+Pininfa@users.noreply.github.com>
2019-06-20 16:03:19 +0400

    Add files via upload

---
+++ test4/lib/owlcarousel/script/owl-video.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+     $(".video-slider-two").owlCarousel({
+           video: true,  
+         
+  });
+}); 
+
+
+
+
+
+
---
+++ test4/lib/owlcarousel/script/owl.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel({
+      loop: true,
+      margin:10,
+
+    responsive:{
+        0:{
+          items:1,  
+        },
+        700:{
+          items:2,  
+        },
+        1024:{
+            items:3,
+        }
+    }
+  });
+}); 
+
+
+        
+
+
+
+
+
+
+
+
+
+
