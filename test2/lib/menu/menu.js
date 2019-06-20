1120d19253929ec81d4a1d5f1b3030e05ae736ac
Pininfa <36874933+Pininfa@users.noreply.github.com>
2019-06-20 09:10:44 +0400

    Add files via upload

---
+++ test2/lib/menu/menu.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){         
+                    
+    $(".icon-menu__bar").click(function(){
+    
+    $(".we").slideToggle();
+    
+    });
+});
+
+                               
+     
+
+
+
+
+
+
+
+/*$('.icon-menu__bar').click(function() {
+  $(this).toggleClass('expanded').siblings('div').slideToggle();
+});*/
